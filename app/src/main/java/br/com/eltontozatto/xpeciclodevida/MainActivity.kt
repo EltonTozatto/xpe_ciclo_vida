afba10201f0c35420e7e550f839de7837fe3231c
Elton Tozatto <eltontozatto@gmail.com>
2023-01-24 21:52:31 -0300

    Trabalhando com o ciclo de vida das activities

--- app/src/main/java/br/com/eltontozatto/xpeciclodevida/MainActivity.kt
+++ app/src/main/java/br/com/eltontozatto/xpeciclodevida/MainActivity.kt
@@ -1,14 +1,64 @@
 package br.com.eltontozatto.xpeciclodevida
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.i("XPELog", "MainActivity::onCreate")
+        Log.i(TAG, "$ACTIVITY onCreate")
+
+        configurarListeners()
+    }
+
+    private fun configurarListeners() {
+        configurarListenerBtnNovaActitivy()
+    }
+
+    private fun configurarListenerBtnNovaActitivy() {
+        val button = findViewById<Button>(R.id.btnNovaTela)
+        button.setOnClickListener() {
+            startActivity(Intent(this, NovaActivity::class.java))
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "$ACTIVITY onResume")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "$ACTIVITY onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "$ACTIVITY onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "$ACTIVITY onRestart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "$ACTIVITY onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "$ACTIVITY onDestroy")
+    }
+
+    companion object {
+        private const val TAG = "XPELog"
+        private const val ACTIVITY = "MainActivity::"
     }
 }
--- app/src/main/java/br/com/eltontozatto/xpeciclodevida/NovaActivity.kt
+++ app/src/main/java/br/com/eltontozatto/xpeciclodevida/NovaActivity.kt
@@ -1,11 +1,76 @@
 package br.com.eltontozatto.xpeciclodevida
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class NovaActivity : AppCompatActivity() {
+    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nova)
+        button = findViewById(R.id.btnVoltar)
+        if (savedInstanceState != null) {
+            if (savedInstanceState.getBoolean(TAG_VIEW)) {
+                button.visibility = View.VISIBLE
+            }
+        }
+        Log.i(TAG, "${ACTIVITY}onCreate")
+        configurarListeners()
+    }
+
+    private fun configurarListeners() {
+        acaoBotaoVoltar()
+        acaoBotaoExibir()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(TAG_VIEW, button.visibility == View.VISIBLE)
+    }
+
+    private fun acaoBotaoExibir() {
+        findViewById<Button>(R.id.btnExibir).setOnClickListener {
+            button.visibility = View.VISIBLE
+        }
+    }
+
+    private fun acaoBotaoVoltar() {
+        button.setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "${ACTIVITY}onResume")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "${ACTIVITY}onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "${ACTIVITY}onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "${ACTIVITY}onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "${ACTIVITY}onDestroy")
+    }
+
+    companion object {
+        private const val TAG = "XPELog"
+        private const val ACTIVITY = "NovaActivity::"
+        private const val TAG_VIEW = "button_visibility"
     }
 }
